ada2a4b448cc99d6c9558e2a6e2ec23600bc1f57
Cameron Sampson <vaerros@gmail.com>
2014-04-02 21:40:20 -0500

    Plugged in AssetPack
    Also made jQuery and Google Maps API resources load from
    protocol-relative paths

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
+require 'sinatra/assetpack'
+
+assets {
+  serve '/css',    { :from => 'assets/css' }
+  serve '/js',     { :from => 'assets/js' }
+  serve '/images', { :from => 'assets/images' }
+
+  css :app, ['css/app.css']
+  js  :app, ['js/spin.min.js', 'js/app.js']
+
+  css_compression :yui
+  js_compression  :uglify
+}
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 get '/' do
   erb :index
 end
-
-
