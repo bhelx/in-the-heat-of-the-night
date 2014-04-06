3046dceffb112cac3366125f583f458ee3393ec5
Cameron Sampson <vaerros@gmail.com>
2014-04-06 03:51:08 -0500

    Moved page spinner options out of controller

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,23 +3,6 @@ angular.module('app.controllers', [])
 .controller('CrimeViewerCtrl', ['$scope', '$q', 'crimeData', function($scope, $q, crimeData) {
   $scope.crimeData = crimeData;
 
-  $scope.spinnerOptions = {
-    lines: 11,
-    length: 23,
-    width: 8,
-    radius: 40,
-    corners: 1,
-    rotate: 9,
-    speed: 1,
-    trail: 50,
-    shadow: true,
-    hwaccel: false,
-    className: "page-content-spinner",
-    color: "#FFF",
-    top: "50%",
-    left: "50%"
-  };
-
   $scope.state = {
     crimeType: 'All',
     loading: true
