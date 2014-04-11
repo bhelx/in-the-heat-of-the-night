30d11cdd87dd255b1b7263de6193a29068c566df
Cameron Sampson <vaerros@gmail.com>
2014-04-10 21:49:58 -0500

    CrimeViewCtrl improvements
    - Filter now operates on array on strings(including 'All' for
      reseting)
    - Relevant scope variables now encapsulated in data obj
    - Spec for map point updating

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,26 +1,48 @@
 angular.module('app.controllers', [])
 
-.controller('CrimeViewerCtrl', ['$scope', '$q', 'crimeData', function($scope, $q, crimeData) {
+.controller('CrimeViewerCtrl', ['$scope', 'crimeData', function($scope, crimeData) {
   $scope.crimeData = crimeData;
 
-  $scope.state = {
-    crimeType: '',
-    loading: true
+  $scope.data = {
+    state: {
+      crimeType: 'All',
+      loading: true
+    },
+    config: {
+      crimeTypeOptions: [],
+      spinnerOptions: {
+        lines:      11,
+        length:     23,
+        width:      8,
+        radius:     40,
+        corners:    1,
+        rotate:     9,
+        speed:      1,
+        trail:      50,
+        shadow:     true,
+        hwaccel:    false,
+        className: 'page-content-spinner',
+        color:     '#FFF',
+        left:      '50%',
+        top:       '50%'
+      }
+    }
   };
 
-  $scope.$watch('state.crimeType', function(value) {
-    if(value) {
-      crimeData.filteredTypes = [value];
+  $scope.$watch('data.state.crimeType', function(value) {
+    if(value === 'All') {
+      crimeData.filteredTypes = [];
       crimeData.updateMapPoints();
     }
     else {
-      crimeData.filteredTypes = [];
+      crimeData.filteredTypes = [value];
       crimeData.updateMapPoints();
     }
   });
 
   // TODO: fetch error handling
   crimeData.fetch().then(function() {
-    $scope.state.loading = false;
+    $scope.data.config.crimeTypeOptions = crimeData.getCrimeTypes().concat('All');
+    $scope.data.state.loading = false;
   });
 }]);
--- assets/js/test/unit/controllers_spec.js
+++ assets/js/test/unit/controllers_spec.js
@@ -13,21 +13,47 @@ describe('crime viewer controller', function() {
     CrimeViewerCtrl = $controller('CrimeViewerCtrl', { $scope: scope });
   }));
 
-  beforeEach(function() {
+  it('should update the filtered crime types array when filtered crime type is reset (filter disabled)', function() {
     $httpBackend.whenGET('/spot_crime').respond([]);
-  });
 
-  it('should update the filtered crime types array when state.crimeType is reset (filter disabled)', function() {
-    scope.state.crimeType = null;
-    scope.$apply();
+    scope.data.state.crimeType = 'All';
+    scope.$digest();
 
     expect(crimeData.filteredTypes).toEqual([]);
   });
 
-  it('should update the filtered crime types array when state.crimeType is a specific value (filter enabled)', function() {
-    scope.state.crimeType = 'Assault';
-    scope.$apply();
+  it('should update the filtered crime types array when filtered crime type is set (filter enabled)', function() {
+    $httpBackend.whenGET('/spot_crime').respond([]);
+
+    scope.data.state.crimeType = 'Assault';
+    scope.$digest();
 
     expect(crimeData.filteredTypes).toEqual(['Assault']);
+  });
+
+  it('should update the map points array when filtered crime type changes', function() {
+    $httpBackend.whenGET('/spot_crime').respond({crimeList: [
+      { cname: 'Assault', clatitude: 0, clongitude: 0 },
+      { cname: 'Burglary', clatitude: 30, clongitude: -30 },
+      { cname: 'Burglary', clatitude: 40, clongitude: -40 }
+    ]});
+
+    $httpBackend.flush();
+
+    scope.data.state.crimeType = 'Assault';
+    scope.$digest();
+
+    expect(crimeData.mapPoints.length).toBe(1);
+    expect(crimeData.mapPoints[0].lat()).toBe(0);
+    expect(crimeData.mapPoints[0].lng()).toBe(0);
+
+    scope.data.state.crimeType = 'Burglary';
+    scope.$digest();
+
+    expect(crimeData.mapPoints.length).toBe(2);
+    expect(crimeData.mapPoints[0].lat()).toBe(30);
+    expect(crimeData.mapPoints[0].lng()).toBe(-30);
+    expect(crimeData.mapPoints[1].lat()).toBe(40);
+    expect(crimeData.mapPoints[1].lng()).toBe(-40);
   });
 });
